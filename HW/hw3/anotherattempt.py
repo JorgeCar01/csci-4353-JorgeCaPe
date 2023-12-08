77854296779ddd65d05a1b8e47d9d3cbc6258b77
JorgeCar01 <112974072+JorgeCar01@users.noreply.github.com>
2023-12-08 18:30:11 +0000

    a

---
+++ HW/hw3/anotherattempt.py
@@ -0,0 +1,170 @@
+# %%
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import matplotlib
+import matplotlib.pylab as plt
+
+from torchvision.utils import make_grid, save_image
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+batch_size = 512
+epochs = 30
+sample_size = 64
+nz = 128
+k = 1
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+path_to_data = r'/home/jorgecarranzapena01/celebA'
+train_dataset = datasets.ImageFolder(root=path_to_data, transform=transform)
+
+
+train_loader = DataLoader(dataset, batch_size, shuffle = True, num_workers=4)
+
+# %%
+class Generator(nn.Module):
+    def __init__(self, nz):
+        super(Generator, self).__init__()
+        self.nz = nz
+        # Adjust the size of layers and final output to match resized image size (224x224)
+        self.main = nn.Sequential(
+            nn.Linear(self.nz, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2048),
+            nn.LeakyReLU(0.2),
+            nn.Linear(2048, 224*224*3),  # Output size to match 224x224x3
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        return self.main(x).view(-1, 3, 224, 224)  # Reshape to match image dimensions
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.n_input = 224 * 224 * 3  # Adjust input size for resized images
+        self.main = nn.Sequential(
+            nn.Linear(self.n_input, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = x.view(-1, self.n_input)  # Flatten image for processing
+        return self.main(x)
+
+
+# %%
+generator = Generator(nz).to(device)
+discriminator = Discriminator().to(device)
+print(generator)
+print(discriminator)
+
+
+# %%
+optim_g = optim.Adam(generator.parameters(), lr=0.0002)
+optim_d = optim.Adam(discriminator.parameters(), lr=0.0002)
+
+criterion = nn.BCELoss()
+
+losses_g = []
+losses_d = []
+images = []
+
+
+# %%
+def save_generator_image(image, path):
+    save_image(image, path)
+
+
+def train_discriminator(optimizer, data_real, data_fake):
+    b_size = data_real.size(0)
+    real_label = torch.ones(b_size, 1).to(device)
+    fake_label = torch.zeros(b_size, 1).to(device)
+    optimizer.zero_grad()
+    output_real = discriminator(data_real)
+    loss_real = criterion(output_real, real_label)
+    output_fake = discriminator(data_fake)
+    loss_fake = criterion(output_fake, fake_label)
+    loss_real.backward()
+    loss_fake.backward()
+    optimizer.step()
+    return loss_real + loss_fake
+
+
+def train_generator(optimizer, data_fake):
+    b_size = data_fake.size(0)
+    real_label = torch.ones(b_size, 1).to(device)
+    optimizer.zero_grad()
+    output = discriminator(data_fake)
+    loss = criterion(output, real_label)
+    loss.backward()
+    optimizer.step()
+    return loss
+
+
+# %%
+generator.train()
+discriminator.train()
+
+import time
+
+since = time.time()
+
+for epoch in range(epochs):
+    loss_g = 0.0
+    loss_d = 0.0
+    for idx, data in tqdm(
+        enumerate(train_loader), total=int(len(train_dataset) / train_loader.batch_size)
+    ):
+        image, _ = data
+        image = image.to(device)
+        b_size = len(image)
+        for step in range(k):
+            data_fake = generator(torch.randn(b_size, nz).to(device)).detach()
+            data_real = image
+            loss_d += train_discriminator(optim_d, data_real, data_fake)
+        data_fake = generator(torch.randn(b_size, nz).to(device))
+        loss_g += train_generator(optim_g, data_fake)
+    generated_img = generator(torch.randn(b_size, nz).to(device)).cpu().detach()
+    generated_img = make_grid(generated_img)
+    if epoch % 5 == 0:
+        save_generator_image(generated_img, f"/home/jorgecarranzapena01/csci-4353-JorgeCaPe/HW/hw3/img/gen_img{epoch}.png")
+        images.append(generated_img)
+    epoch_loss_g = loss_g / idx
+    epoch_loss_d = loss_d / idx
+    losses_g.append(epoch_loss_g)
+    losses_d.append(epoch_loss_d)
+
+    print(f"Epoch {epoch} of {epochs}")
+    print(f"Generator loss: {epoch_loss_g:.8f}, Discriminator loss: {epoch_loss_d:.8f}")
+
+time_elapsed = time.time() - since
+print(time_elapsed // 60, "min", time_elapsed % 60, "sec")
+
+for i in range(10):
+    generated_img = generator(torch.randn(b_size, nz).to(device)).cpu().detach()
+    generated_img = make_grid(generated_img)
+    save_generator_image(generated_img, f"/home/jorgecarranzapena01/csci-4353-JorgeCaPe/HW/hw3/img/outputted_img{i + 1}.png")
+
